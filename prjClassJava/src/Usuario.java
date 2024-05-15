021a9d96d906385e3ae4a9a6c74e05c595faf39f
Keyaru <118556238+keyaru18@users.noreply.github.com>
2024-05-15 12:39:34 -0500

    sisi seguimos

--- prjClassJava/src/Usuario.java
+++ prjClassJava/src/Usuario.java
@@ -1,3 +1,21 @@
 public class Usuario {
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean login (){
+        System.out.println("Ready for use");
+        return true ;
+    }
+
+    public void createDino(){
+        
+    }
+   
 }
