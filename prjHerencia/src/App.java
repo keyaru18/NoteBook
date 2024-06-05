86e595533358ff661ffa96794fdf8515891c4c9a
Keyaru <118556238+keyaru18@users.noreply.github.com>
2024-06-05 12:54:30 -0500

    Agregar clase de 5 de junio del 2024

---
+++ prjHerencia/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+            Padre p = new Padre("Keyaru");
+            Hijo h = new Hijo("Vanitas");
+            p.saludar();
+            h.saludar();
+    }
+}
