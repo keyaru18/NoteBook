2157a4999efe212d3e10d135b1eb6b48fcf032ad
Keyaru <118556238+keyaru18@users.noreply.github.com>
2024-06-11 12:56:18 -0500

    Agregar clase del 11-06-2024

---
+++ prjInterface/src/animal/Mosca.java
@@ -0,0 +1,8 @@
+package animal;
+
+public class Mosca {
+    public Mosca(String nombre) {
+        setNombre(nombre);
+        System.out.println("Nació una mosca: "+ getNombre());
+    }
+}
---
+++ prjInterface/src/clasificacionAnimal/Insectos.java
@@ -0,0 +1,14 @@
+package clasificacionAnimal;
+
+public abstract class Insectos extends Invertebrados{
+    private Integer nroPatas;
+
+    public Integer getNroPatas() {
+        return nroPatas;
+    }
+
+    public void setNroPatas(Integer nroPatas) {
+        this.nroPatas = nroPatas;
+    }
+
+}
